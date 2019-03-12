3ea77a1b0c502f3a7d9bff171a7d23b1a5901528
jorgepaz96 <pazmissael@gmail.com>
2019-03-11 21:34:58 -0500

    first commit

---
+++ services/users/project/config.py
@@ -0,0 +1,18 @@
+class BaseConfig:
+   """Configuracion base"""
+   TESTING = False
+
+
+class DevelopmentConfig(BaseConfig):
+   """Configuraccion de desarrollo"""
+   pass
+
+
+class TestingConfig(BaseConfig):
+   """Configuración de prueba"""
+   pass
+
+
+class ProductionConfig(BaseConfig):
+   """Configuracion de produccion"""
+   pass
